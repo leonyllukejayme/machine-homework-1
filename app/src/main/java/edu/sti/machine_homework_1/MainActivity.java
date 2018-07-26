db83d573b6a72240249c8639f474089c4ef9f8c3
leonyllukejayme <leonyljayme@gmail.com>
2018-07-26 21:32:28 +0800

    Assignment passed

---
+++ app/src/main/java/edu/sti/machine_homework_1/MainActivity.java
@@ -0,0 +1,13 @@
+package edu.sti.machine_homework_1;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.layout.layout.activity_main);
+    }
+}
